79c9ad511a1a73f1c0f2904622f3ac77cc931b9d
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-06-13 15:22:14 +0900

    refac .gitignore

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from twitter import *
 
 app = Flask(__name__)
 app.config.from_pyfile('./app.cfg', silent=True)
@@ -6,7 +7,15 @@ app.config.from_pyfile('./app.cfg', silent=True)
 
 @app.route("/")
 def index():
-    return "hello!!"
+    auth = OAuth(
+        consumer_key=app.config['CONSUMER_KEY'],
+        consumer_secret=app.config['CONSUMER_SECRET'],
+        token=app.config['TOKEN'],
+        token_secret=app.config['TOKEN_SECRET']
+    )
+    twitter_userstream = TwitterStream(auth=auth, domain='userstream.twitter.com')
+    for msg in twitter_userstream.user():
+        print(msg)
 
 if __name__ == "__main__":
     app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
