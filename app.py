e2ca93025662c49f82f00a58cb82a82418f3be38
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-06-15 17:00:44 +0900

    v1.0.0

---
+++ app.py
@@ -0,0 +1,85 @@
+async_mode = None
+
+if async_mode is None:
+    try:
+        import eventlet
+        async_mode = 'eventlet'
+    except ImportError:
+        pass
+
+    if async_mode is None:
+        try:
+            from gevent import monkey
+            async_mode = 'gevent'
+        except ImportError:
+            pass
+
+    if async_mode is None:
+        async_mode = 'threading'
+
+    print('async_mode is ' + async_mode)
+
+if async_mode == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+elif async_mode == 'gevent':
+    from gevent import monkey
+    monkey.patch_all()
+
+import json
+import time
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+from tweepy.streaming import StreamListener, Stream
+import tweepy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config.from_pyfile('./app.cfg', silent=True)
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+
+auth = tweepy.OAuthHandler(app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+auth.set_access_token(app.config['TOKEN'], app.config['TOKEN_SECRET'])
+
+
+class StdOutListner(StreamListener):
+    def __init__(self):
+        pass
+
+    def on_data(self, raw_data):
+        try:
+            tweet = json.loads(raw_data)
+            print(tweet)
+            socketio.emit('stream_channel',
+                          {'data': tweet},
+                          namespace='/user_streaming')
+        except ValueError:
+            pass
+
+    def on_error(self, status_code):
+        print('Error status code', status_code)
+        exit()
+
+
+def background():
+    stream = Stream(auth, l, secure=True)
+    stream.userstream()
+
+
+@app.route('/')
+def index():
+    global thread
+    if thread is None:
+        thread = Thread(target=background)
+        thread.daemon = True
+        thread.start()
+    return render_template('index.html')
+
+l = StdOutListner()
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='127.0.0.1')
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Electron側の初期設定
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -6,36 +5,36 @@ let mainWindow;
 
 // アプリを閉じた時にquit
 app.on('window-all-closed', function() {
-	  app.quit();
+  app.quit();
 });
 
 // アプリ起動後の処理
 app.on('ready', function() {
-	var subpy = require('child_process').spawn('python',['./index.py']);
-	var rq = require('request-promise');
-	var mainAddr = 'http://localhost:5000';
+  var subpy = require('child_process').spawn('python',['./app.py']);
+  var rq = require('request-promise');
+  var mainAddr = 'http://localhost:5000';
 
-	var openWindow = function() {
-		mainWindow = new BrowserWindow({width: 400, height: 300 });
-		mainWindow.loadURL(mainAddr);
+  var openWindow = function() {
+    mainWindow = new BrowserWindow({width: 400, height: 300 });
+    mainWindow.loadURL(mainAddr);
 
-		// 終了処理
-		mainWindow.on('closed', function() {
-			mainWindow = null;
-			subpy.kill('SIGINT');
-		});
-	};
+    // 終了処理
+    mainWindow.on('closed', function() {
+      mainWindow = null;
+      subpy.kill('SIGINT');
+    });
+  };
 
-	var startUp = function() {
-		rq(mainAddr)
-			.then(function(htmlString) {
-				console.log('server started');
-				openWindow();
-			})
-			.catch(function(err) {
-				startUp();
-			});
-	};
+  var startUp = function() {
+    rq(mainAddr)
+      .then(function(htmlString) {
+        console.log('server started');
+        openWindow();
+      })
+      .catch(function(err) {
+        startUp();
+      });
+  };
 
-	startUp();
+  startUp();
 });
